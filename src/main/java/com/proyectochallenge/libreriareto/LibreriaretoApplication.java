afa557a98a62b8f11fd36765e0a021dfe76c6252
VittorBit <victorsupo.262002@gmail.com>
2024-07-07 22:07:47 -0500

    Crear programa de libreria web

---
+++ src/main/java/com/proyectochallenge/libreriareto/LibreriaretoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectochallenge.libreriareto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaretoApplication.class, args);
+	}
+
+}
